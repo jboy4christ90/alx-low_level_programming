21acf3811987a68986c50819fd182bb968513c6b
Joseph <aceeee90@gmail.com>
2023-06-15 10:01:21 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    printf("Art, ");
+    printf("but ");
+    printf("with ");
+    printf("proper ");
+    printf("grammar, ");
+    printf("is ");
+    printf("a ");
+    printf("piece ");
+    printf("of ");
+    printf("art,");
+    printf(", ");
+    printf("followed ");
+    printf("by ");
+    printf("a ");
+    printf("new ");
+    printf("line.");
+    printf("\n");
+    return 0;
+}
+
