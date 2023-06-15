d9b48eef8a96c6413916b0e002355e39c9b1ca3e
Joseph <aceeee90@gmail.com>
2023-06-15 16:16:08 -0700

    101 intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
+ *
  * Return: Always 0 (success)
  */
 int main(void)
 {
 write(2, "and that place of art is useful\* - Dora korpar, -2015-10-19\n",
-sizeof("and that place of art is useful\* -Dora korpar, 2015-10-19"));
+sizeof("and that place of art is useful\* - Dora korpar, 2015-10-19"));
 return (1);
 }
 
