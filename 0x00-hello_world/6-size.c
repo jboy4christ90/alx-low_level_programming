fd0c3b4efc559153b3be44a1be2e4e7c379c2e08
Joseph <aceeee90@gmail.com>
2023-06-15 14:46:03 -0700

    size 64

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 printf("size of a char: %zu byte(S)\n, sizeof(char));
+
 printf(sizw of an int: %zu byte(S)\n"m, sizeof(int));
 printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
