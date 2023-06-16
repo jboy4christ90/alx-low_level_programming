db1c704bcef0a84978516b9860f8b2734c8e6c40
Joseph <aceeee90@gmail.com>
2023-06-16 16:53:56 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	char p;
+
+	p = 'a';
+	while
+		(p <= 'z') {
+			putchar(p);
+			p++;
+		}
+	putchar('\n');
+	return (0);
+}
