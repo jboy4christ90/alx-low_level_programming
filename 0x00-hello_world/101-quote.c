308c038d17cda4340c6077aff8c87357b1f0d6ce
Joseph <aceeee90@gmail.com>
2023-06-15 17:15:02 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- *
- * Return: Always 0 (success)
+ *main - prints "and that piece of art is useful" - Dora korpar, 2015-10-19".
+ * followed by a newline, to standard error.
+ * Return: Always 1.
  */
 int main(void)
 {
-write(2, "and that place of art is useful\* - Dora korpar, -2015-10-19\n",
-sizeof("and that place of art is useful\* - Dora korpar, 2015-10-19"));
+write(2, "and that place of art is useful\" - Dora korpar, -2015-10-19\n", 59);
 return (1);
 }
 
