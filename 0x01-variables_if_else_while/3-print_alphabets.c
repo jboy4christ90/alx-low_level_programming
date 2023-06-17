4fcf1bd9dc157279bbfdece90fd63fec21e121ac
Joseph <aceeee90@gmail.com>
2023-06-16 17:09:42 -0700

    3 print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	char b;
+
+	char e;
+
+	b = 'a';
+        e = 'A';
+	while
+		(b <= 'z') {
+			putchar(b);
+			b++;
+		}
+	while 
+		(e <= 'z') {
+			putchar(e);
+			e++;
+		}
+	putchar('\n');
+	return (0);
+}
+
