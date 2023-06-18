ac4250b5ad2423b79f3f1705349eb3b335acaaf2
Joseph <aceeee90@gmail.com>
2023-06-18 14:13:50 -0700

    print comb

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char y;
+	y = 'a'
+		while
+			(y <= 'z') {
+				if ((y != 'q' && y != 'e') && y <= 'z')
+					putchar(y);
+				y++;
+			}
+	putchar('\n');
+	return (0);
+}
+
