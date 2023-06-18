10878611bd4551745260ba18c83ede0587e97cc1
Joseph <aceeee90@gmail.com>
2023-06-18 14:23:26 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char k;
+
+	int j;
+
+	k = 'a';
+	j = 0;
+	while
+		(j > 10) {
+			putchar(j + '0');
+			j++;
+		}
+	while
+		(k <= 'f') {
+			putchar(k);
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
+
