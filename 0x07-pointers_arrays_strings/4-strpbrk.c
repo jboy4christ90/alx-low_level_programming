2c208a086e026a7d99d1349411bc204889882e39
Joseph <aceeee90@gmail.com>
2023-07-03 19:11:40 -0700

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * _strpbrk _ Entry point
  * @s: imnput
@@ -9,16 +8,14 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, n;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (n = 0; accept[n] != '\0'; n++)
-		{
-			if (s[i] == accept[n])
-				return (s + i);
-		}
-	}
-
-	return (NULL);
+int i, n;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (n = 0; accept[n] != '\0'; n++)
+{
+if (s[i] == accept[n])
+return (s + i);
+}
+}
+return (NULL);
 }
