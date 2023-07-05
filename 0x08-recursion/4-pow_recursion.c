9ebf9a19f16b0898ae0d53aa3eec583ffe88a706
Joseph <aceeee90@gmail.com>
2023-07-05 11:32:05 -0700

    4 pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,13 +6,11 @@
  *
  * Return: integer value
  */
-int factorial(int n)
+int _pow_recursion(int x, int y)
 {
-	if (n < 0)
+	if (y < 0)
 		return (-1);
-
-	if (n <= 1)
+	if (y == 1)
 		return (1);
-
-	return (n * factorial(n -1));
+	return (x * _pow_recursion(x, y - 1));
 }
